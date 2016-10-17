e004dee458faa6713c411c793adc20ff4f243f52
Michael Sharp <msharp@amplience.com>
2016-10-17 15:05:15 +0100

    trying to set up tests... styles and assets still don't load

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,27 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material'; 
 import { AppComponent } from './app.component';
 
 describe('App: Ng2TestMaterial', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
+    declarations: [
+      AppComponent
+    ],
+    imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule, 
+      MaterialModule.forRoot()
+    ],
+    providers: []
+    })
   });
 
   it('should create the app', async(() => {
